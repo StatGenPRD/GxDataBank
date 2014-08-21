dd3794b9b018b60a28b08113f718f4cc02109a07
Andrew Slater <andrew.j.slater@gsk.com>
2014-08-21 12:28:34 -0400

    broke read_through_header function out of gxbank_convert so could be used in gxbank_add

---
+++ bankfunctions.py
@@ -0,0 +1,6 @@
+#function to read through headers and return first non-header line
+def read_through_headers(file,prefix) :
+	line = file.readline()
+	while line[0:len(prefix)] == prefix :
+		line = file.readline()
+	return line
